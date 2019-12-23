3e70bdbba75179d27ad338aa25b7ca4c484e9aca
haitien <haitienakhs@gmail.com>
2019-12-23 16:25:34 +0700

    add login_form and implement login function

--- student_management/controller/user_controller.py
+++ student_management/controller/user_controller.py
@@ -1,3 +1,19 @@
+from model import User
+
+
 class UserController(base_controller.BaseController):
-    def __init__(self):
+    def __init__(self, params):
         pass
+        self.user_ = User(params)
+
+    def login(user_name, password):
+        if not _validate_login():
+            return False
+        return self.user_.login()
+
+    def _validate_login(user_name, password):
+        if user_name is None:
+            return False
+        if password is None:
+            return False
+        return True
--- student_management/model/user.py
+++ student_management/model/user.py
@@ -1,3 +1,7 @@
 class User(object):
-    def __init__(self):
+    def __init__(self, params):
+        pass
+
+    def login(user_name, password):
+        login_query = "select * from User where userName=@user_name, password=@password"
         pass
