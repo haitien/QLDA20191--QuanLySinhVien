ea888bbbb43516ca0c9ad0ee5ff43b79253570f4
haitien <haitienakhs@gmail.com>
2019-12-24 15:59:48 +0700

    Thêm chức năng đký học phần.

---
+++ sources/student_management/controller/subject_controller.py
@@ -0,0 +1,7 @@
+from sources.student_management.controller import base_controller
+
+class SubjectController(base_controller.BaseController):
+    def __init__(self):
+        pass
+    def register_subject(self, user):
+        pass
