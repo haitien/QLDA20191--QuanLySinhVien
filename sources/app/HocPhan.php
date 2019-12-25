1ebc735596aa930dca0ccf4fa3d3bd5e7de11d14
dunghustk60 <tranminhdungbk@gmai.com>
2019-12-25 09:54:44 +0700

    fix

--- sources/app/HocPhan.php
+++ sources/app/HocPhan.php
@@ -8,6 +8,7 @@ class HocPhan extends Model
 {
     protected $table = 'hocphan';
     public $timestamps = false;
+    public $timestamps = false;
 
     public function lophoc() {
     	return $this->belongsTo('App\LopHoc','maHocPhan','maLop');
