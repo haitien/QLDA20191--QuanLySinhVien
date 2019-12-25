1f7636c245a278d994045fa93a0f3d3a5078c9eb
dunghustk60 <tranminhdungbk@gmai.com>
2019-12-25 09:41:04 +0700

    them chuc nang tim ma lop

--- sources/app/BangDiemLop.php
+++ sources/app/BangDiemLop.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class BangDiemLop extends Model
 {
+	public function timMaLop(){
+		//
+	}
     protected $table = 'bangdiemlop';
     public $timestamps = false;
 }
