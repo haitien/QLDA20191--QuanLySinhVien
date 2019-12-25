179bacba58a539185a6b7d4c1f61649d6ce26a42
dunghustk60 <tranminhdungbk@gmai.com>
2019-12-25 09:43:18 +0700

    them chuc nang thong ke diem

--- sources/app/BangDiemSV.php
+++ sources/app/BangDiemSV.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class BangDiemSV extends Model
 {
+	public function thongKeDiem(){
+		///
+	}
     protected $table = 'bangdiemsv';
     public $timestamps = false;
 }
