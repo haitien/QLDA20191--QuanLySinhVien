017ebb7351389f7e16bb32fae4f42b5e5834997a
dunghustk60 <tranminhdungbk@gmail.com>
2019-12-27 20:19:11 +0700

    them chuc nang

--- sources/app/DotDangKy.php
+++ sources/app/DotDangKy.php
@@ -14,6 +14,29 @@ class DotDangKy extends Model
     public function chophep() {
     	return $this->chophep;
     }
+     public function diemchu($diemqt, $diemck, $trongso) {
+        $diemchu = $diemqt*(1-$trongso) + $diemck*$trongso;
+        if($diemchu<4 OR $diemqt<3 OR $diemck <3) {
+            return 'F';
+        } elseif($diemchu<5) {
+             $diem = 'D';
+        }   elseif ($diemchu<5.5) {
+                $diem = 'D+';
+        }   elseif ($diemchu<6.5) {
+                $diem = 'C';
+        }   elseif ($diemchu<7) {
+                $diem = 'C+';
+        }   elseif ($diemchu<8) {
+                $diem = 'B';
+        }   elseif ($diemchu<8.5) {
+                $diem = 'B+';
+        }   elseif ($diemchu<9.5) {
+                $diem = 'A';
+        }   else {
+                $diem = 'A+';
+        }
+        return $diem;
+    }
 public function timMahocPhan(){
     ///
 }
