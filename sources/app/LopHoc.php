e997128e5bc0d1c47c0b63fe441f1c0c3807a84b
dunghustk60 <tranminhdungbk@gmail.com>
2019-12-27 20:16:11 +0700

    commit

--- sources/app/LopHoc.php
+++ sources/app/LopHoc.php
@@ -10,7 +10,29 @@ class LopHoc extends Model
     protected $primaryKey = 'maLop';
     public $incrementing = false;
     public $timestamps = false;
-
+ public function diemchu($diemqt, $diemck, $trongso) {
+        $diemchu = $diemqt*(1-$trongso) + $diemck*$trongso;
+        if($diemchu<4 OR $diemqt<3 OR $diemck <3) {
+            return 'F';
+        } elseif($diemchu<5) {
+             $diem = 'D';
+        }   elseif ($diemchu<5.5) {
+                $diem = 'D+';
+        }   elseif ($diemchu<6.5) {
+                $diem = 'C';
+        }   elseif ($diemchu<7) {
+                $diem = 'C+';
+        }   elseif ($diemchu<8) {
+                $diem = 'B';
+        }   elseif ($diemchu<8.5) {
+                $diem = 'B+';
+        }   elseif ($diemchu<9.5) {
+                $diem = 'A';
+        }   else {
+                $diem = 'A+';
+        }
+        return $diem;
+    }
     public function bangdiem() {
     	return $this->hasMany('App\BangDiem','maLop','id');
     }
