acf54675fad55158453d65b88146528763ae924e
dunghustk60 <tranminhdungbk@gmail.com>
2019-12-27 19:58:35 +0700

    them chuc nang

--- sources/app/User.php
+++ sources/app/User.php
@@ -36,5 +36,28 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function diemchu($diemqt, $diemck, $trongso) {
+        $diemchu = $diemqt*(1-$trongso) + $diemck*$trongso;
+        if($diemchu<4 OR $diemqt<3 OR $diemck <3) {
+            return 'F';
+        } elseif($diemchu<5) {
+             $diem = 'D';
+        }   elseif ($diemchu<5.5) {
+                $diem = 'D+';
+        }   elseif ($diemchu<6.5) {
+                $diem = 'C';
+        }   elseif ($diemchu<7) {
+                $diem = 'C+';
+        }   elseif ($diemchu<8) {
+                $diem = 'B';
+        }   elseif ($diemchu<8.5) {
+                $diem = 'B+';
+        }   elseif ($diemchu<9.5) {
+                $diem = 'A';
+        }   else {
+                $diem = 'A+';
+        }
+        return $diem;
+    }
 
 }
