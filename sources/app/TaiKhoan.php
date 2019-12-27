df1b24e0cc9ca6c8d0703654cab6b58a67f89e10
dunghustk60 <tranminhdungbk@gmail.com>
2019-12-27 20:06:10 +0700

    them chuc nang

--- sources/app/TaiKhoan.php
+++ sources/app/TaiKhoan.php
@@ -7,12 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class TaiKhoan extends Authenticatable
+
 {
     use Notifiable;
     protected $table = 'taikhoan';
     protected $primaryKey = 'username';
     public $incrementing = false;
     public $timestamps = false;
+    public function diemchu($diemqt, $diemck, $trongso) {
+        $diemchu = $diemqt*(1-$trongso) + $diemck*$trongso;
+        if($diemchu<4 OR $diemqt<3 OR $diemck <3) {
+            return 'F';
+        } elseif($diemchu<5) {
+             $diem = 'D';
+        }   elseif ($diemchu<5.5) {
+                $diem = 'D+';
+        }   elseif ($diemchu<6.5) {
+                $diem = 'C';
+        }   elseif ($diemchu<7) {
+                $diem = 'C+';
+        }   elseif ($diemchu<8) {
+                $diem = 'B';
+        }   elseif ($diemchu<8.5) {
+                $diem = 'B+';
+        }   elseif ($diemchu<9.5) {
+                $diem = 'A';
+        }   else {
+                $diem = 'A+';
+        }
+        return $diem;
+    }
 
     protected $fillable = [
          'hoTen', 'username', 'password'
